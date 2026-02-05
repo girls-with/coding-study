cb84d553d1d60e2c1c9a780c85d856df3ce75d5b
eunseo <dmstjesp@gmail.com>
2026-02-06 06:29:12 +0900

    BOJ_17478.py

--- eunseo/solved/fibonacci.py
+++ eunseo/solved/fibonacci.py
@@ -1,6 +1,6 @@
 N = int(input())
-a = [0, 1] 
+a = [0, 1] # 초기값 설정
 for i in range(2, N+1): 
-        a.append(a[i-1] + a[i-2])
+        a.append(a[i-1] + a[i-2]) # 피보나치는 2부터 시작하므로 범위는 2~N
 
-print(a[N])    
+print(a[N]) # N번째 원소 구하기
