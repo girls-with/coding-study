21750fe4c531fb7e7954d71cf84d0352066e0f75
sherry-107 <chaseoyoung13@gmail.com>
2026-04-19 00:10:40 +0900

    [solved] [SWEA-23802] 화분과 비료 - 차서영

--- seoyeong/solved/SWEA_23802.py
+++ seoyeong/solved/SWEA_23802.py
@@ -1,18 +1,21 @@
 t = int(input())
- 
+
 for tc in range(1, t+1):
     n, p = map(int, input().split())
- 
+
     A_biryo = list(map(int, input().split()))
     B_biryo = list(map(int, input().split()))
- 
-    plant = [[0]*2 for i in range(n)]
- 
-    plant[0][0] = A_biryo[0]
-    plant[0][1] = B_biryo[0]
- 
+
+    plant = [[0, 0] for i in range(n)] #화분의 성장을 기록할 리스트
+    # n*2형태의 리스트. 
+    plant[0][0] = A_biryo[0] #0열은 A비료
+    plant[0][1] = B_biryo[0] #1열은 B비료
+
     for i in range(1, n):
         plant[i][0] = max(plant[i-1][0] + A_biryo[i] - p, plant[i-1][1] + A_biryo[i])
         plant[i][1] = max(plant[i-1][0] + B_biryo[i], plant[i-1][1] + B_biryo[i] - p)
- 
-    print(f'#{tc} {max(plant[n-1][0], plant[n-1][1])}')
+        #A비료를 줄 때와 B비료를 줄 때를 각각 기록
+        #만약 p만큼 덜 자라는 것을 감수하고도 똑같은 비료를 주는 것이 좋은지, 아니면 다른 비료를 주는 것이 좋은지 비교
+    result = max(plant[-1][0], plant[-1][1]) #최댓값 기록
+
+    print(f'#{tc} {result}')
